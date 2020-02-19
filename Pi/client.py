e80f9faa01661599b242a2ba63c8b6285b62f715
Nicklas Rouvinen <nicklas.rouvinen@gmail.com>
2020-02-19 13:30:14 -0600

    new client and server using multiprocessing and threading

--- Pi/client.py
+++ Pi/client.py
@@ -1,6 +1,46 @@
-import sys
 import socket
+import sys
+import time
+import threading
+from Pi.led_controls import LED
+from neopixel import *
 
+SOCKET_AMOUNT = 100
+HOST = ''
+PORT = 12345
+SIZE = 1024
+
+"""Testing new client and server configurations using multi processing and threading"""
+
+def myclient(ip, port, message):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Socket created")
+    except socket.error as err:
+        print("Socket creation failed with error {}".format(err))
+
+    s.connect((ip, port))
+    s.sendall(message.encode())
+    from_server = s.recv(SIZE)
+    print("{0} final client time {1}".format(from_server.decode(), time.time()))
+    s.close()
+
+if __name__ == "__main__":
+    threads = []
+    for i in range(SOCKET_AMOUNT):
+        msg = "Thread# {}, client time {}".format(i, time.time())
+        client_thread = threading.Thread(target=myclient, args=(HOST, PORT, msg))
+        threads.append(client_thread)
+        client_thread.start()
+
+    waiting = time.time()
+    [x.join() for x in threads]
+    done = time.time()
+    print("Done {}. Waiting for {} seconds".format(done, done-waiting))
+
+
+
+"""
 try:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Socket created")
@@ -8,7 +48,7 @@ except socket.error as err:
     print("Socket creation failed with error {}".format(err))
 
 port = 12345
-host_ip = '10.0.0.28'
+host_ip = ''
 
 client.connect((host_ip, port))
 
@@ -16,8 +56,22 @@ print("socket connected to {}".format(host_ip))
 
 client.send("Client connected".encode())
 
+"""
+run this command in python terminal when in the rpi_ws281x/python directory
+sudo PYTHONPATH=".:build/lib.linux-armv7l-2.7" python examples/strandtest.py
+"""
+
+
+strip = LED()
+
 while True:
     from_server = client.recv(1024)
     print(from_server.decode())
+    if from_server == 'Green':
+        strip.colorWipe(Color(255, 0, 0))
+
+
 
 client.close()
+
+"""
--- Pi/led_controls.py
+++ Pi/led_controls.py
@@ -1,12 +1,22 @@
-import board
-import neopixel
+from neopixel import *
 
 
 class LED:
-    def __init__(self, pixel_pin, num_pixels, brightness, auto_write, order):
-        self.pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=brightness,
-                                        auto_write=auto_write, pixel_order=order)
+    LED_COUNT = 78
+    LED_PIN = 18
+    LED_FREQ_HZ = 800000
+    LED_DMA = 10
+    LED_INVERT = False
+    LED_BRIGHTNESS = 150
+    LED_CHANNEL = 0
 
-    def fill_color(self, rgb: tuple) -> None:
-        self.pixels.fill(rgb)
+    def __init__(self):
+        self.strip = Adafruit_NeoPixel(self.LED_COUNT, self.LED_PIN, self.LED_FREQ_HZ, self.LED_DMA,
+                                       self.LED_INVERT, self.LED_BRIGHTNESS, self.LED_CHANNEL)
+        self.strip.begin()
 
+    def colorWipe(self, color):
+        """Wipe color across display a pixel at a time."""
+        for i in range(self.strip.numPixels()):
+            self.strip.setPixelColor(i, color)
+            self.strip.show()
